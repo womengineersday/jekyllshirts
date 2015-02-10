b3b0f32b96b5f45cfb56d8efebcb0d81b6f9c2bb
Alex Klibisz <aklibisz@vols.utk.edu>
2015-02-10 17:20:56 -0500

    Create gallery-randomize.js

---
+++ js/gallery-randomize.js
@@ -0,0 +1,23 @@
+//politely borrowed from: http://stackoverflow.com/questions/1533910/randomize-a-sequence-of-div-elements-with-jquery
+
+$( document ).ready(function() {
+    $(".gallery").randomize("", "a");
+});
+
+(function($) {
+  
+  $.fn.randomize = function(tree, childElem) {
+    return this.each(function() {
+      var $this = $(this);
+      if (tree) $this = $(this).find(tree);
+      var unsortedElems = $this.children(childElem);
+      var elems = unsortedElems.clone();
+      
+      elems.sort(function() { return (Math.round(Math.random())-0.5); });  
+
+      for(var i=0; i < elems.length; i++)
+        unsortedElems.eq(i).replaceWith(elems[i]);
+    });    
+  };
+
+})(jQuery);
